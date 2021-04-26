577ebbfbc8b0fb12996e3a3a1a12b72d3e08c237
user195594 <user195594@telehouse-moboware.dmarc.si1.atlanticmetro.net>
2021-04-26 10:48:02 -0400

    first commit

---
+++ I am Rich2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich2
+//
+//  Created by user195594 on 4/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
